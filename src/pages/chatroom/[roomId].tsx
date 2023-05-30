ecf58b43971ab0d6b03b31cf34535b905ad3069e
Erard Valdez <valdez.erardtimothy@gmail.com>
2023-05-31 04:44:38 +0800

    chatroom Room ID dynamic route

--- src/pages/chatroom/[roomId].tsx
+++ src/pages/chatroom/[roomId].tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import React from 'react';
 
 
 export default function ChatRoom() {
   return ( 
     <>
+      <Head>
+        <title>Chat Room</title>
+      </Head>
       <h2>Work in Progress.</h2>
       <p>Come back soon.</p>
     </>
