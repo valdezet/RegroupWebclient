a7ef3247b8c34e52a3288a51802a3d6cbad635a2
Erard Valdez <valdez.erardtimothy@gmail.com>
2023-05-31 04:45:18 +0800

    add application/json default headers for axios instance

--- src/api/axiosApiFactory.ts
+++ src/api/axiosApiFactory.ts
@@ -2,8 +2,10 @@ import axios, { AxiosInstance } from "axios";
 
 export default function getBackendAxiosInstance(): AxiosInstance {
   const instance : AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   });
+  instance.defaults.headers.post["Content-Type"] = "Application/json";
+  instance.defaults.headers.common["Accept"] = "Application/json";
 
   return instance;
     
